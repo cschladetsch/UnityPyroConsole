739fc04a2ce42add9828dd08355bfab1e052cc6e
Christian <christian.schladetsch@gmail.com>
2019-11-18 23:48:08 +1100

    Code review and script rename.

---
+++ PyroConsole/Assets/App/Scripts/MoveBetweenTwoLocations.cs
@@ -0,0 +1,38 @@
+﻿namespace App
+{
+    using UnityEngine;
+    using CoLib;
+
+    /// <summary>
+    /// Move attached object between two transforms in given time.
+    /// </summary>
+    public class MoveBetweenTwoLocations : MonoBehaviour
+    {
+        public Transform Begin, End;
+        public float TransitionTime = 1;
+
+        private readonly CommandQueue _queue = new CommandQueue();
+
+        private void Start()
+        {
+            var tr = transform;
+            var st = Begin;
+            var position = st.position;
+            tr.position = position;
+            tr.rotation = st.rotation;
+
+            _queue.Sequence(
+                Cmd.RepeatForever(
+                    Cmd.MoveTo(tr, End.position, TransitionTime, Ease.InOutQuad()),
+                    Cmd.MoveTo(tr, position, TransitionTime, Ease.InOutQuad())
+                )
+            );
+        }
+
+        private void Update()
+        {
+            _queue.Update(Time.deltaTime);
+        }
+    }
+}
+
